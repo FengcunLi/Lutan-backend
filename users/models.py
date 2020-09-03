8888aa09be847041f97300c35f0b8eb1e89a3115
Robert <robert_lexis@163.com>
2020-09-03 19:02:27 +0800

    First Commmit.

---
+++ users/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from avatars import upload_to
+from django.contrib.postgres.fields.jsonb import JSONField
+
+
+class User(AbstractUser):
+    """Lutan User Model Class
+
+    User.is_staff: 
+        Designates whether the user can log into admin sites.
+    User.is_active: 
+        Designates whether this user should be treated as active.
+        Unselect this instead of deleting accounts.
+
+    Args:
+        models ([type]): [description]
+    """
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    avatar_tmp = models.ImageField(
+        max_length=255, upload_to=upload_to, null=True, blank=True
+    )
+    avatar_src = models.ImageField(
+        max_length=255, upload_to=upload_to, null=True, blank=True
+    )
+    avatar_crop = models.CharField(max_length=255, null=True, blank=True)
+    avatars = JSONField(null=True, blank=True)
+
+    signature = models.CharField(max_length=255, null=True, blank=True)
+
+    followings = models.ManyToManyField(
+        "self", related_name="followers", symmetrical=False,
+    )
+
+    blockings = models.ManyToManyField(
+        "self", related_name="blockers", symmetrical=False
+    )
+
+
+class UserProxy(User):
+    class Meta:
+        proxy = True
